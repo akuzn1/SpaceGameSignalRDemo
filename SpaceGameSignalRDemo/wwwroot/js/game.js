99d4b4774aad11df6f45d28c921bcd22dbf2712f
Aleksandr Kuznetsov <akuzn1@gmail.com>
2018-10-20 07:07:48 +0400

    initial js was added

--- SpaceGameSignalRDemo/wwwroot/js/game.js
+++ SpaceGameSignalRDemo/wwwroot/js/game.js
@@ -1,1 +1,28 @@
-﻿
+﻿var Game = (function () {
+	var mainCanvas = document.getElementById("mainCanvas");
+	var background = new Image();              
+
+	return {
+		init: function () {
+			window.addEventListener('resize', this.resizeCanvas, false);
+			background.src = 'images/backgrounds/space1.jpg';			
+			background.onload = function () {
+				Game.resizeCanvas();
+			}
+		},
+
+		redraw: function () {
+			var ctx = mainCanvas.getContext('2d');
+			ctx.drawImage(background,
+				0, 0, background.naturalHeight, background.naturalHeight,
+				0, 0, mainCanvas.width,	mainCanvas.width * background.naturalHeight / background.naturalHeight);
+		},
+
+		resizeCanvas: function () {
+			mainCanvas.width = window.innerWidth;
+			mainCanvas.height = window.innerHeight;
+			Game.redraw();
+		},
+	}
+})();
+Game.init();
